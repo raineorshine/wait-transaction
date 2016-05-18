1efe675861963f7a3468af0cf6779d720b7305f0
Raine Revere <raineorshine@gmail.com>
2016-05-18 16:40:40 -0600

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-export default function (web3) {
+export default function (web3, options = {}) {
+  options.maxAttempts = options.maxAttempts || 240
+  options.timeInterval = options.timeInterval || 1000
+
   return function (...args) {
-    var txParams = {}
-
-    // It's only txParams if it's an object and not a BigNumber.
-    // var lastArg = args[args.length - 1]
-    // if (Pudding.is_object(lastArg) && lastArg instanceof Pudding.BigNumber == false) {
-    //   txParams = args.pop()
-    // }
-    txParams = args.pop()
-
-    // txParams = merge(Pudding.class_defaults, self.class_defaults, txParams)
-
     return new Promise(function (resolve, reject) {
-      var callback = function (error, tx) {
-        var interval = null
-        var maxAttempts = 240
-        var attempts = 0
+      const callback = function (error, tx) {
+        let interval
+        let attempts = 0
 
         if (error) {
           reject(error)
           return
         }
 
-        var makeAttempt = function () {
+        const makeAttempt = function () {
           web3.eth.getTransaction(tx, function (e, txInfo) {
             // If there's an error ignore it.
             if (e) {
@@ -34,21 +25,20 @@ export default function (web3) {
               resolve(tx)
             }
 
-            if (attempts >= maxAttempts) {
+            if (attempts >= options.maxAttempts) {
               clearInterval(interval)
               reject(new Error('Transaction ' + tx + ' wasn\'t processed in ' + attempts + ' attempts!'))
             }
 
-            attempts += 1
+            attempts++
           })
         }
 
-        interval = setInterval(makeAttempt, 1000)
+        interval = setInterval(makeAttempt, options.timeInterval)
         makeAttempt()
       }
 
-      args.push(txParams, callback)
-      web3.eth.sendTransaction(...args)
+      web3.eth.sendTransaction(...[...args, callback])
     })
   }
 }
