3f3cbe4a8205bae09c69d02fe4bdc8d1a4ff7b11
Raine Revere <raineorshine@gmail.com>
2016-05-19 10:30:04 -0600

    es6-ify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-module.exports = function (web3, options = {}) {
+module.exports = (web3, options = {}) => {
   options.maxAttempts = options.maxAttempts || 240
   options.timeInterval = options.timeInterval || 1000
 
-  return function (...args) {
-    return new Promise(function (resolve, reject) {
-      const callback = function (error, tx) {
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      const callback = (error, tx) => {
         let interval
         let attempts = 0
 
@@ -13,14 +13,11 @@ module.exports = function (web3, options = {}) {
           return
         }
 
-        const makeAttempt = function () {
-          web3.eth.getTransaction(tx, function (e, txInfo) {
-            // If there's an error ignore it.
-            if (e) {
-              return
-            }
+        const makeAttempt = () => {
+          web3.eth.getTransaction(tx, (e, { blockHash }) => {
+            if (e) { return }
 
-            if (txInfo.blockHash && txInfo.blockHash) {
+            if (blockHash) {
               clearInterval(interval)
               resolve(tx)
             }
